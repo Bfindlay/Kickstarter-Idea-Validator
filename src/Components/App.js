244e2d0b38798534f6f8898b853df3362c5ef002
Bfindlay <bfin5330@uni.sydney.edu.au>
2018-08-04 13:05:06 +1000

    added ML dataset

--- src/Components/App.js
+++ src/Components/App.js
@@ -16,7 +16,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1> Guten Tag! </h1>
+                <h1> Hallo </h1>
             </div>
         )
     }
