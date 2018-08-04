da7246bca923557076354619fc1c382499c714e6
Bfindlay <bfin5330@uni.sydney.edu.au>
2018-08-04 13:36:52 +1000

    removed authentication middleware

--- API.js
+++ API.js
@@ -13,33 +13,7 @@ Router.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 //API Verification MiddleWare
 Router.use(function(req, res, next) {
     const { body, method, url } = req;
-    if(method === 'POST'){
-        let token = body.token;
-        if(url === '/auth/login' || url === '/auth/register'){
-            return next();
-        }
-        if (token !== undefined) {
-            jwt.verify(token, SECRET_TOKEN, function(err, decoded) {      
-                if (err) {
-                    console.log('failed to authenticate token');
-                    return res.status(403).send({ success: false, message: 'Failed to authenticate token.' });
-                } else {
-                    console.log('authenticated token');
-                    req.decoded = decoded;
-                    return next();
-                }
-            });
-        } else {
-            console.log('body does not contain auth code');
-            return res.status(403).send({ 
-                success: false, 
-                message: 'No token provided.' 
-            });
-        }
-    }else{
-        // No token needed
-        return next();
-    }
+    return next();
 }); 
 
 
