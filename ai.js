0dadca2bc253e2e23ea500cc2250b075c343b308
Liam Chiang <jchi9176@google.com>
2018-08-04 18:39:56 +1000

    new files

--- ai.js
+++ ai.js
@@ -3,8 +3,10 @@ var nbc = Classifier();
 var nlp = require('wink-nlp-utils');
 var fs = require('fs');
 var stream = fs.createReadStream('Data/ks-projects-201801.csv');
+var stream2 = fs.createReadStream('Data/ks-projects-201801 (With duration).csv');
 var csv = require("fast-csv");
- 
+
+
 nbc.definePrepTasks([
     nlp.string.tokenize0,
     nlp.tokens.removeWords,  
@@ -23,23 +25,57 @@ var std_devCal = (data) => {
     var sum = 0;
     var mean = meanCal(data);
     for(var i = 0; i = len(data); ++i){
-        sum += data[i] - mean;
+        sum += (data[i] - mean)*(data[i] - mean);
     }
-    return sum/len(data);
+    return Math.sqrt(sum/len(data));
 };
 
-var pdf = () => {
-
+var pdf = (target, mean, std) => {
+    return (1/(std * Math.sqrt(2 * Math.PI))) * Math.exp(-(target - mean)*(target - mean)/(2*std*std));
 };
+
 nbc.defineConfig( { considerOnlyPresence: true, smoothingFactor: 0.5 });
 var goal_success = [];
 var goal_failure = [];
+var pledged_success = [];
+var pledged_failure = [];
+var backers_success = [];
+var backers_failure = [];
+var usd_pledged_success = [];
+var usd_pledged_failure = [];
+var usd_pledged_real_success = [];
+var usd_pledged_real_failure = [];
+var usd_goal_real_success = [];
+var usd_goal_real_failure = [];
+
+//stage1
 csv.fromStream(stream, {headers : true}).on("data", function(data){
-    const { name, main_category , goal, state, country} = data;
+    const { name, main_category} = data;
     nbc.learn( name, main_category );
+
+}).on("end", function(){
+    nbc.consolidate();
+    console.log("done");
+    console.log(nbc.predict("pet feeding dog walking with human"));
+    console.log(nbc.predict("cat food fishing"));
+    console.log(nbc.predict("andriod computer"));
+    console.log(nbc.predict("draaw books testing studio"));
+    console.log(nbc.predict("music sound facter"));
+    console.log(nbc.predict("pet feeding dog walking with cat"));
+    console.log(nbc.predict("pet feeding dog walking with turtle"));
+    console.log(nbc.predict("cat dog turtle animal vet"));
+});
+
+//stage2
+csv.fromStream(stream2, {headers : true}).on("data", function(data){
+    const { name, main_category, goal, state, country} = data;
+    const {goal, state, pledged, } = data;
+    nbc.learn( name, main_category );
+    //successful state
     if(state == "successful"){
         goal_success.push(goal);
     }
+    //failed state
     else if(state == "failed"){
         goal_failure.push(goal);
     }
